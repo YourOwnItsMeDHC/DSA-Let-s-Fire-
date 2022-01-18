e0aca4c8ba49ef104b3ad0faabb55266073c042f
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-01-18 14:28:57 +0530

    Circumference And Area Of Circle

---
+++ Functions/src/com/functions/CircumferenceAndAreaOfCircle.java
@@ -0,0 +1,32 @@
+package com.functions;
+
+import java.util.Scanner;
+
+public class CircumferenceAndAreaOfCircle {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        //Program to print circumference and area of a circle of radius entered by user by defining your own method.
+        //Area = PI * r^2
+        //Circumference = 2 * PI * r
+
+        System.out.print("Enter the Radius : ");
+        double radius = in.nextDouble();
+
+        System.out.println(area(radius));
+
+        System.out.println(circumference(radius));
+
+    }
+
+    static double area(double r){
+        System.out.print("Area of radius = " + r + " is : ");
+        return Math.PI * (r * r);
+        }
+
+    static double circumference(double r) {
+        System.out.print("Circumference of radius = " + r + " is : ");
+        return 2 * Math.PI * r;
+    }
+
+    }
