bebf02ecac93489c8ea150c722b62cfe464d81fa
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-01-19 14:32:36 +0530

    Sum Of First N Natural Numbers

---
+++ Functions/src/com/functions/SumOfNNaturalNumbers.java
@@ -0,0 +1,33 @@
+package com.functions;
+
+import java.util.Scanner;
+
+public class SumOfNNaturalNumbers {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        //Write a function that returns the sum of first n natural numbers
+
+        System.out.print("Sum of how many first N natural numbers you want : ");
+        int num = in.nextInt();
+
+        System.out.println(sumOfNatural(num));
+    }
+
+    static int sumOfNatural(int n){
+        int i, sum = 0;
+        for(i=1; i<=n; i++){
+            sum += i;
+
+            //line 22 to line 27 is just to beautify output
+            if(i == n){
+                System.out.println(i + " = ");
+                continue;
+            }
+            System.out.print(i + " + ");
+
+        }
+
+        return sum;
+    }
+}
