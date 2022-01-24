cf1a3ba404a51c8f84c55f6960cd482ab6092922
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-01-24 23:58:10 +0530

    Print 2D Array Using "for" loop

--- Arrays/Basics/src/com/company/Print2dArray.java
+++ Arrays/Basics/src/com/company/Print2dArray.java
@@ -1,21 +1,20 @@
 package com.company;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-public class Print2Darray {
+public class Print2dArray {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
-        //Print elements in 2D Array
+        //Print 2D Array
 
         int[][] arr = new int[3][3];
 
         System.out.println("Enter the values for 2D Array : ");
 
         //Take Input
-        for(int row=0; row<arr.length; row++){                 //arr.length will give number of rows
-            for(int col=0; col<arr[row].length; col++){       //arr[row}.length will give number of columns of that particular row
+        for(int row=0; row<arr.length; row++){
+            for(int col=0; col<arr[row].length; col++){
                 arr[row][col] = in.nextInt();
             }
         }
