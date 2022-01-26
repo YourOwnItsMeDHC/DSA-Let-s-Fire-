61562cf5452ce8f55d98e81e329464537c705e11
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-01-26 16:42:23 +0530

    Swap

--- Arrays/Basics/src/com/company/Swap.java
+++ Arrays/Basics/src/com/company/Swap.java
@@ -11,20 +11,21 @@ public class Swap {
 
         int[] arr = new int[5];
 
-        System.out.println("Enter 5 values for Array : ");
+        System.out.print("Enter " + arr.length + " values for Array : ");
+
         for(int i=0; i<arr.length; i++){
             arr[i] = in.nextInt();
         }
 
         System.out.println(Arrays.toString(arr));
 
-        System.out.println("Enter Two Index Value : ");
+        System.out.print("Enter Two Index Value : ");
         int in1 = in.nextInt();
         int in2 = in.nextInt();
 
         swap(arr, in1, in2);
 
-        System.out.println(Arrays.toString(arr));
+        System.out.println("Swapped Array is : " + Arrays.toString(arr));
 
     }
     static void swap(int[] array, int index1, int index2){
