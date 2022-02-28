478834b388d0326561528a34d8838e70afa7b7ce
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-02-28 17:01:44 +0530

    Search Target and Return Index

---
+++ Searching/Linear_Search_Basics/src/com/company/SearchIndex.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SearchIndex {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Size of Array : ");
+        int n = in.nextInt();
+
+        int[] nums = new int[n];
+
+        for(int i=0; i<nums.length; i++) {
+            System.out.print("Enter Element For Index " + i + ": ");
+            nums[i] = in.nextInt();
+        }
+
+        System.out.print("Which Element You Would Like To Search : ");
+        int target = in.nextInt();
+
+        int ans = linearSearch(nums, target);
+        System.out.print(target + " is present at Index : " + ans);
+    }
+
+    static int linearSearch(int[] nums, int target) {
+        if(nums.length == 0) {
+            return -1;
+        }
+
+        for (int i = 0; i <nums.length ; i++) {
+            if(nums[i] == target) {
+                return i;
+            }
+        }
+
+        //If after searching in whole Array, Element not found i.e. not present, so just return -1
+        return -1;
+    }
+}
