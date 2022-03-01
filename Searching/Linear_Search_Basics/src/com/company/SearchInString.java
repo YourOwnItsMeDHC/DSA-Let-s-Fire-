980ad63067690198cc055b3ad3deeb66c6813ee3
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-03-01 13:46:29 +0530

    Q1_Search in String

---
+++ Searching/Linear_Search_Basics/src/com/company/SearchInString.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class SearchInString {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter A String : ");
+        String str = in.next().trim();
+
+        System.out.print("Character You Wanna Search : ");
+        char letter = in.next().trim().charAt(0);
+
+        int length = str.length();
+
+        int position = -1;
+        for (int i = 0; i <length-1 ; i++) {
+            if(letter == str.charAt(i)) {
+                position = i+1;
+                System.out.println(letter + " is present at position : " + position);
+//                break;
+                //If break is uncommented it will return all occurrences of letter
+                //If break is commented it will return only the first occurrence of letter
+            }
+        }
+        if(position == -1) {
+            System.out.println(letter + " is not present in " + str);
+        }
+    }
+}
