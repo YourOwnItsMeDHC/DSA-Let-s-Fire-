3abc4d4b6b7ee8e68fc607ab08d0bb404c976646
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-03-05 15:02:42 +0530

    Q3(a)_Minimum Number

--- Searching/Linear_Search_Basics/src/com/company/MinNumber.java
+++ Searching/Linear_Search_Basics/src/com/company/MinNumber.java
@@ -22,7 +22,7 @@ public class MinNumber {
 
     }
     static int searchMin(int[] nums) {
-        //Assuming, length of an Array is not zero
+        //Assuming , length of an Array is not zero
         int min = nums[0];
         for (int i = 1; i < nums.length; i++) {
             if(nums[i] < min) {
