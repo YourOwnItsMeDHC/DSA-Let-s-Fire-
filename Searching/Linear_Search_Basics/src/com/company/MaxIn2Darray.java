ca058c79067ce311ea470eb19da86ef97940c7fb
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-03-06 14:39:36 +0530

    Q4(b)_Max in 2D Array

---
+++ Searching/Linear_Search_Basics/src/com/company/MaxIn2Darray.java
@@ -0,0 +1,54 @@
+package com.company;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class MaxIn2Darray {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Array is : ");
+        int[][] num = {
+                {4, 654, 45, 5445},
+                {74, 687, 7486, 574, 54},
+                {5465, 54},
+                {5},
+                {244, 45, 44}
+        };
+
+        for(int[] row : num) {
+            System.out.println(Arrays.toString(row));
+        }
+        System.out.println();
+
+        System.out.println("Initially Before Traversing Through 2D Array is : " + Integer.MIN_VALUE);
+
+        System.out.println("Max Element is : " + max(num));
+    }
+
+//    static int max(int[][] num) {
+//        int max = Integer.MIN_VALUE;              //Initially, or max = [0][0] i.e. first element in an Array
+//        for (int row = 0; row < num.length; row++) {
+//            for (int col = 0; col< num[row].length; col++) {
+//                if(num[row][col] > max) {
+//                   max = num[row][col];
+//                }
+//            }
+//        }
+//        return max;
+//    }
+
+
+    //Using "enhanced for' loop :
+    static int max(int[][] num) {
+        int max = Integer.MIN_VALUE;              //Initially, or max = [0][0] i.e. first element in an Array
+        for (int[] row : num) {
+            for (int elementInRow : row) {
+                if(elementInRow > max) {
+                    max = elementInRow;
+                }
+            }
+        }
+        return max;
+    }
+}
