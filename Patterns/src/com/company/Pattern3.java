5a6becdd404ceab8248d7ca7a2be769d061c978b
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-08-08 02:10:43 +0530

    Downward Triangle Star Pattern

--- Patterns/src/com/company/Pattern3.java
+++ Patterns/src/com/company/Pattern3.java
@@ -16,17 +16,41 @@ package com.company;
 public class Pattern3 {
     public static void main(String[] args) {
         pattern3(5);
+        System.out.println();
+        pattern3a(5);
     }
 
+//    Method - 01 :
     static void pattern3(int n) {
-//        for(int row=n; row>=1; row--) {         //outer loop for number of rows(n)
-//            for(int col=1; col<=row; col++) {   //  inner loop for columns
-        for(int row=1; row<=n; row++) {
-            for(int col=1; col<=n-row+1; col++) {
+        for (int row = n; row >= 1; row--) {         //outer loop for number of rows(n)
+            for (int col = 1; col <= row; col++) {   //  inner loop for columns
                 System.out.print("* ");
             }
             System.out.println();               // ending line after each row
         }
     }
 
+
+
+
+
+
+//    Method - 02 :
+/*
+n=5
+
+* * * * *      row=0 ,   col = n - row = 5 - 0 = 5
+* * * *        row=1 ,   col = n - row = 5 - 1 = 4
+* * *          row=2 ,   col = n - row = 5 - 2 = 3
+* *            row=3 ,   col = n - row = 5 - 3 = 2
+*              row=4 ,   col = n - row = 5 - 4 = 1
+     */
+    static void pattern3a(int n) {
+        for(int row=0; row<n; row++) {
+            for (int col=0; col <(n-row); col++) {
+                System.out.print("* ");
+            }
+            System.out.println();               // ending line after each row
+        }
+    }
 }
