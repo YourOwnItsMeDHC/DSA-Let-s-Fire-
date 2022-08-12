79c2fd0bb0d26d925cad3867acc9523c0d30b193
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-08-12 22:47:34 +0530

    Diamond Pattern (Numbers) (Refer-Pattern-28)

---
+++ Patterns/src/com/company/RPattern30.java
@@ -0,0 +1,41 @@
+/* Same as Pattern-28 : Diamond Shape Pattern (👈👈👈👈Firstly See These)
+https://github.com/YourOwnItsMeDHC/DSA-Let-s-Fire-/blob/master/Patterns/src/com/company/Pattern5.java
+
+Diamond Pattern (Numbers) :
+
+        1
+      2 1 2
+    3 2 1 2 3
+  4 3 2 1 2 3 4
+5 4 3 2 1 2 3 4 5
+
+ */
+
+
+package com.company;
+public class RPattern30 {
+    public static void main(String[] args) {
+        pattern30(5);
+    }
+
+    static void pattern30(int n) {
+        for(int row=1; row<=n; row++) {
+
+            int spaces = n-row;
+            for(int s=1; s<=spaces; s++) {
+                System.out.print("  ");
+            }
+
+            for(int col=row; col>=1; col--) {
+                System.out.print(col + " ");
+            }
+
+            for(int col=2; col<=row; col++) {
+                System.out.print(col + " ");
+            }
+
+            System.out.println();
+        }
+    }
+
+}
