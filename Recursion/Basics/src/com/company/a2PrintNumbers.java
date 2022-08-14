8fc66d58ef89fae8ba216146f3334e3737c7f972
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-08-14 15:26:36 +0530

    Print numbers 1 to 5 using function

---
+++ Recursion/Basics/src/com/company/a2PrintNumbers.java
@@ -0,0 +1,30 @@
+package com.company;
+public class a2PrintNumbers {
+    public static void main(String[] args) {
+        number1(1);
+    }
+
+    static void number1(int n) {
+        System.out.println(n);
+        number2(2);
+    }
+
+    static void number2(int n) {
+        System.out.println(n);
+        number3(3);
+    }
+
+    static void number3(int n) {
+        System.out.println(n);
+        number4(4);
+    }
+
+    static void number4(int n) {
+        System.out.println(n);
+        number5(5);
+    }
+
+    static void number5(int n) {
+        System.out.println(n);
+    }
+}
