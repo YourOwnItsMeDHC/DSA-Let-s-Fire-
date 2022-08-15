c227577f307194345b830c2b90c757326ba3073d
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-08-15 23:49:18 +0530

    Fibonacci Number Using RECURSION

---
+++ Recursion/Basics/src/com/company/a4FibonacciNumber.java
@@ -0,0 +1,19 @@
+//Fibonacci Number : https://github.com/YourOwnItsMeDHC/ProgramBasics/blob/master/src/deepak/FibonacciNumber.java
+//Fibonacci Series : https://github.com/YourOwnItsMeDHC/ProgramBasics/blob/master/src/deepak/FibonacciSeries.java
+
+package com.company;
+public class a4FibonacciNumber {
+    public static void main(String[] args) {
+        int ans = fibonacci(6);
+        System.out.println(ans);
+    }
+
+    static int fibonacci(int n) {
+        if(n < 2) {
+            return n;
+        }
+
+        return fibonacci(n-1) + fibonacci(n-2);
+    }
+
+}
