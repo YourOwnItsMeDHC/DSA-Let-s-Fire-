5b8f7581fe8f94f1d1055ce3a79e1e9cd787199f
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-09-25 01:02:51 +0530

    Find Unique Number i.e. unpaired (Must Check Q2)

---
+++ Bitwise & Maths/src/com/company/Q2NonRepeatingNumber2.java
@@ -0,0 +1,24 @@
+//Must check Q2
+//Question :
+//In an array, only one element is there which is not havin pair i.e. only positive value of that is present
+// and rest of the elements are having pairs i.e. both positive as well as negative value of that is present
+//Find that one element which is unique
+
+package com.company;
+public class Q2NonRepeatingNumber2 {
+    public static void main(String[] args) {
+        int[] nums = {-1, 3, 2, -2, 4, -4, 10, 1, -3};
+        System.out.println(uniqueElm(nums));                 //10
+    }
+
+    private static int uniqueElm(int[] nums) {
+        int unique = 0;
+
+        for(int n : nums) {
+            unique += n;
+        }
+
+        return unique;
+    }
+
+}
