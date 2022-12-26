7b6ad2d0928500069fbdd8c64176bda340e7c042
YourOwnItsMeDHC <2019c08@sigce.edu.in>
2022-12-26 22:54:54 +0530

    1342. Number of Steps to Reduce a Number to Zero

---
+++ Recursion/Basics/src/com/company/c2CountZero.java
@@ -0,0 +1,26 @@
+package com.company;
+
+public class c2CountZero {
+    public static void main(String[] args) {
+        System.out.println(count(2030405));
+    }
+
+    static int count(int n) {
+        //Here, we have created extra "helper" function, because we need an extra argument for "count'
+        return helper(n, 0);
+    }
+
+    static int helper(int n, int count) {
+        if(n == 0) {
+            return count;
+        }
+
+        int rem = n%10;
+        if(rem == 0) {
+            return helper(n/10, ++count);
+        }
+        return helper(n/10, count);
+
+    }
+
+}
